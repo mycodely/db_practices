cd7a071e97685558bac8aef1033d6598989d8c34
mycodely <122876501+mycodely@users.noreply.github.com>
2023-06-07 18:00:21 -0300

    Update asdDB.sql

--- asdDB.sql
+++ asdDB.sql
@@ -24,3 +24,32 @@ ALTER TABLE animales ADD edad INT;
 ALTER TABLE animales ADD sexo varchar(1);
 ALTER TABLE animales ADD nombre varchar(20);
 
+CREATE table user(
+id int not null auto_increment,
+name varchar(50) not null,
+edad int not null,
+email varchar(50) not null,
+primary key (id)
+);
+
+
+insert into user (name, edad, email) values ('Osacar', 25, 'oscard@gmail.com');
+insert into user (name, edad, email) values ('Lenia', 15, 'linea@gmail.com');
+insert into user (name, edad, email) values ('Nicolas', 35, 'niconicolas@gmail.com');
+insert into user (name, edad, email) values ('Gimena', 45, 'gimenas@gmail.com');
+
+-- delete from user;
+SELECT * FROM user;
+SELECT * FROM user limit 2;
+select * from user where edad > 15;
+select * from user where edad >= 15;
+select * from user where edad > 30 and email = 'niconicolas@gmail.com';
+select * from user where edad >= 36 or email = 'linea@gmail.com';
+select * from user where edad between 15 and 30;
+select * from user where email like '%gmail%';
+select * from user where email like '%gmail';
+select * from user where email like 'oscar%';
+
+select * from user order by edad asc;
+select * from user order by edad desc;
+select min(edad) as minimo from user;
